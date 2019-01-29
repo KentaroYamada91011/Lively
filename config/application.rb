7fd5cfbf0b791b58c6f085ff5c92f08a0d7d93a0
Kentaro91011 <kentaro.10.11@icloud.com>
2019-01-29 18:41:55 +0900

    change layout

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,8 @@ module IndieProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false  
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
