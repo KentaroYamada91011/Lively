ae38a6adfc7ebfacb761e599e78115963ecb8982
Kentaro91011 <kentaro.10.11@icloud.com>
2019-01-29 18:59:49 +0900

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_01_17_060042) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "image"
     t.datetime "created_at", null: false
@@ -76,6 +81,11 @@ ActiveRecord::Schema.define(version: 2019_01_17_060042) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "image"
     t.datetime "created_at", null: false
