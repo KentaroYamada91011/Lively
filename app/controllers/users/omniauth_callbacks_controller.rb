b3bea64813ba92fb318f2ffd7dd914f0d228c298
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-04 16:42:29 +0900

    init

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,26 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+#class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    callback_from :facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 
   def twitter
-    callback_from :twitter
-  end
-
-  private
-
-  def callback_from(provider)
-    provider = provider.to_s
-
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    # You need to implement the method below in your model
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
   end
