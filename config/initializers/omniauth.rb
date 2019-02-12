d5f075842d0ebe9d3411dde0c2f52fad896bebe2
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-12 12:05:50 +0900

    update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
+OmniAuth.config.full_host = "https://lively-project.herokuapp.com/ # フロントのURL
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  configure do |config|
-    config.full_host = "https://lively-project.herokuapp.com/"
-  end
 end
