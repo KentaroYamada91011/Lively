165c66715aead63e2c887e682941eabac565caa1
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-12 11:36:55 +0900

    update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,7 +280,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"], callback_url: "https://lively-project.herokuapp.com/"
+  config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"], callback_url: "https://lively-project.herokuapp.com/", token_params: { parse: :json }
 
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
