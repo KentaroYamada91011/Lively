967d6089529623899a73f9c31e34354d3526a6a2
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-14 15:52:37 +0900

    update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -59,7 +59,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   # common callback method
-  def callback_for(twitter)
+  def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
