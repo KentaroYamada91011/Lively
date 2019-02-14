4721896e7f6e720b5987dafa36bfe77e0e8409d0
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-14 20:05:14 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,41 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def twitter
+    callback_from :twitter
+  end
+
+  def failure
+    @notice = "failure"
+    redirect_to events_path, notice: 'failure'
+  end
+
+
+  private
+
+  def callback_from(provider)
+    provider = provider.to_s
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      print("persisted true")
+      @notice = "persisted true"
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      print("persisted false")
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      @notice = "user is not persisted."
+      redirect_to root_path, notice: 'user is not persisted.'
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    redirect_to events_path
+  end
+
+  end
 
 private
   def user_params
