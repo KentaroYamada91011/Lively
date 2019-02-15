97dfa261b2e618e3493023753bddbdc47fcfdbad
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-15 13:17:34 +0900

    update

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,5 +9,35 @@ class Artist < ApplicationRecord
   # validates :name, presence: true
   mount_uploader :image, ImageUploader
 
+  def self.find_for_oauth2(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        name:     auth.info.nickname,
+        image:    auth.info.image
+      )
+    end
+    return user
+  end
+
+  def sign_in_and_redirect(resource_or_scope, *args)
+    options  = args.extract_options!
+    scope    = Devise::Mapping.find_scope!(resource_or_scope)
+    resource = args.last || resource_or_scope
+    sign_in(scope, resource, options)
+    redirect_to after_sign_in_path_for(resource)
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
 
 end
