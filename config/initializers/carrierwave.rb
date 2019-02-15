81beb7efbb52d10fe7a6f44e9f58f2ac92d98ea5
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-15 14:50:36 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,13 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAIILL4XN5KLL5QXTQ',
-    aws_secret_access_key: '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',
+    aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1',
-    host:                  's3.amazonaws.com/geek-rails-photo-sampler',             # optional, defaults to nil
+    host:                  's3.amazonaws.com/livelyproject',             # optional, defaults to nil
     endpoint:              'https://s3.ap-northeast-1.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'geek-rails-photo-sampler'                # required
+  config.fog_directory  = 'livelyproject'                # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
