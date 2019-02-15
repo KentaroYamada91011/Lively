d5921d761e9f69afa47d51a55ea299989d3d8674
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-15 12:59:54 +0900

    update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.full_host = "https://lively-project.herokuapp.com" # フロン�
 # require 'omniauth/strategies/twitter_another'
 Rails.application.config.middleware.use OmniAuth::Builder do
   # require 'omniauth/strategies/twitter_another'
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],info_fields:"nickname, image"
-  on_failure { |env| AuthenticationsController.action(:failure).call(env) }
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],info_fields:"nickname, image", path_prefix: '/auth/twitter', callback_path: '/auth/twitter/callback'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],info_fields:"nickname, image", path_prefix: '/auth/twitter_another', callback_path: '/auth/twitter_another/callback'
 end
