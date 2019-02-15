ec8be7d1e6035eaa3eecd4bbb190bc527051fcd8
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-15 10:41:45 +0900

    update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.full_host = "https://lively-project.herokuapp.com" # フロントのURL
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  require 'omniauth/strategies/facebook_another'
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],info_fields:"nickname, image"
   on_failure { |env| AuthenticationsController.action(:failure).call(env) }
+  require 'omniauth/strategies/facebook_another'
 end
