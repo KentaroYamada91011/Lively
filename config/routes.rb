e5f51b52938c332c7cf04f950979362b81fa2148
Kentaro91011 <kentaro.10.11@icloud.com>
2019-02-17 12:28:09 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   root :to => 'events#home'
 
-  # get 'likes/destroy'
   devise_for :artists, controllers: {
     # registrations: 'artists/registrations',
     # sessions: 'artists/sessions',
     # passwords: 'artists/passwords'
   }
-  # get '/auth/twitter_another' => "authentication#twitter"
   get '/auth/another/twitter/callback' => 'anothers#twitter'
   devise_for :users, controllers: {
     # registrations: 'users/registrations',
@@ -16,7 +14,6 @@ Rails.application.routes.draw do
     # passwords: 'users/passwords',
   }
   get "/auth/twitter/callback" => "omniauth_callbacks#twitter"
-
   resources :users
   resources :artists do
     resources :follows ,only:[:create, :destroy]
@@ -28,17 +25,5 @@ Rails.application.routes.draw do
   end
   get "events/terms_of_service"=> "events#terms_of_service"
   get "events/privacy_policy"=> "events#privacy_policy"
-  # get 'artist/:id' => 'artists#show'
-  # delete 'events/:id' => 'events#destroy'
-  # get 'events/:id/edit' => 'events#edit'
-  # patch 'events/:id' => 'events#update'
-  # get 'events/:id' => 'events#show'
-
-  # post "events/:event_id/likes/create" => "likes#create"
-  # delete 'likes/:event_id' => 'likes#destroy'
-
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
